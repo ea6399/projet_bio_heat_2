e1258e762f9253087dd4fdc42a95a94d867c1f09
ALI Emin <eminali2430@gmail.com>
2025-04-10 10:05:11 +0200

    Initialise les varialbles

--- numerics.f90
+++ numerics.f90
@@ -7,17 +7,17 @@ MODULE numerics
       ! Paramètres de résolution et discrétisation
       !------------------------------------------
       integer, parameter :: dp = kind(1.d0)  ! Double précision
-      integer :: M = 16                      ! Niveau de résolution des ondelettes (2^M points)
-      integer :: N = 2 * M                   ! Nombre total de points de collocation (N = 2*M)
-      real(dp) :: dt = 0.01d0                ! Pas de temps Δt
-      real(dp) :: epsilon = 1.0d0            ! Coefficient de diffusion ε
+      integer, parameter :: M = 4                      ! Niveau de résolution des ondelettes (2^M points)
+      integer, parameter :: N = 2 ** M                  ! Nombre total de points de collocation (N = 2*M)
+      real(dp) :: dt = 0.01d0                                   ! Pas de temps Δt
+      real(dp), parameter :: epsilon = 1.0d0            ! Coefficient de diffusion ε
 
       !------------------------------------------
       ! Domaine spatial et temporel
       !------------------------------------------
-      real(dp) :: x_min = 0.0d0, x_max = 1.0d0  ! Domaine en x
-      real(dp) :: y_min = 0.0d0, y_max = 1.0d0  ! Domaine en y
-      real(dp) :: T_total = 1.0d0               ! Temps final
+      real(dp), parameter :: x_min = 0.0d0, x_max = 1.0d0  ! Domaine en x
+      real(dp), parameter :: y_min = 0.0d0, y_max = 1.0d0  ! Domaine en y
+      real(dp), parameter :: T_total = 1.0d0               ! Temps final
 
       !------------------------------------------
       ! Points de collocation et maillages
@@ -55,6 +55,43 @@ MODULE numerics
       real(dp), allocatable :: A_mat(:,:)    ! Matrice du système linéaire
       real(dp), allocatable :: B_vec(:)      ! Vecteur second membre
       integer, allocatable :: ipiv(:)        ! Pivots pour la résolution (LAPACK)
+
+
+
+      contains
+
+
+       subroutine initialize_globals()
+        implicit none
+        integer :: i, r
+        real(dp) :: dx, dy
+        integer :: num_points
+
+        ! Initialisation
+        num_points = ceiling(T_total/dt) + 1
+
+
+        ! Allocation des tableaux
+        allocate(x_c(N), y_c(N), t_grid(num_points))
+        allocate(h(N, N), p1(N, N), p2(N, N), C(N))
+        allocate(a_ij(N, N), b_ij(N, N))
+        allocate(phi(N, N), psi(N, N), f_exact(N, N))
+        allocate(A_mat(N*N, N*N), B_vec(N*N), ipiv(N*N))
+
+        ! Initialisation des points de collocation
+        dx = (x_max - x_min)/N
+        dy = (y_max - y_min)/N
+        do r = 1, N
+            x_c(r) = x_min + (r - 0.5d0)*dx
+            y_c(r) = y_min + (r - 0.5d0)*dy
+        end do
+
+        ! Initialisation de la grille temporelle
+        do i = 0, num_points - 1
+            t_grid(i+1) = i*dt
+        end do
+
+    end subroutine initialize_globals
 
 
 END MODULE numerics
