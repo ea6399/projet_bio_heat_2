85043725308e95a8f956f2d3181fbe2ae509c3c5
ALI Emin <eminali2430@gmail.com>
2025-04-10 12:03:38 +0200

    Calcul des coefficients h, p1 et p2

--- main.f90
+++ main.f90
@@ -1,60 +1,71 @@
 PROGRAM main
 
-      USE numerics
-      USE haar_functions
-      USE matrix
+    USE numerics
+    USE haar_functions
+    USE matrix
 
-      IMPLICIT NONE
-      ! Déclaration des variables locales
-      integer :: i, j
+    IMPLICIT NONE
+    ! Déclaration des variables locales
+    integer :: i, j
 
 
-                  ! ------------------------------ !
-                  !          Début du code         !
-                  ! ------------------------------ !
+                    ! ------------------------------ !
+                    !          Début du code         !
+                    ! ------------------------------ !
+
+        
+
+        ! This is the main program
+        !Initialisation des variables
+        CALL initialize_globals()
+        
+        write(*, '(/,T5,A,/)') " Toutes les variables globales ont été initialisées avec succès !"
+
+        ! Affichage de points de collocation
+        ! write(*, '(/,T5,A,/)') "Points de collocation :"
+        DO i = 1, N
+            write(*, '(1X, F10.5, 1X, F10.5)') x_c(i), y_c(i)
+        END DO
+
+        ! Initialisation des ondelettes de Haar
+        DO j = 1, N
+            DO i = 1, N
+                hx(i,j) = haar(i, x_c(j), x_min, x_max, N)
+                hy(i,j) = haar(i, y_c(j), y_min, y_max, N)
+            END DO
+        END DO
+
+        ! Affichage des ondelettes de Haar
+        write(*, '(/,T5,A,/)') "Fonctions Haar calcule :"
+        call print_matrix(hx)
+        call print_matrix(hy)
+
+        ! Affichage des intégrales premières
+        write(*, '(/,T5,A,/)') "Intégrales premières calcules :"
+        DO j = 1, N
+            DO i = 1, N
+                p1x(i,j) = P_haar(x_c(j), i, 1, x_min, x_max, N)
+                p1y(i,j) = P_haar(y_c(j), i, 1, y_min, y_max, N)
+            END DO
+        END DO
+        call print_matrix(p1x)
+        call print_matrix(p1y)
+
+        ! Affichage des intégrales secondes
+        write(*, '(/,T5,A,/)') "Intégrales secondes calculees :"
+        DO j = 1, N
+            DO i = 1, N
+                    p2x(i,j) = P_haar(x_c(j), i, 2, x_min, x_max, N)
+                    p2y(i,j) = P_haar(y_c(j), i, 2, y_min, y_max, N)
+            END DO
+        END DO
+        call print_matrix(p2x)
+        call print_matrix(p2y)
+
+
+        
 
       
-
-      ! This is the main program
-      !Initialisation des variables
-      CALL initialize_globals()
-      
-      write(*, '(/,T5,A,/)') " Toutes les variables globales ont été initialisées avec succès !"
-
-      ! Affichage de points de collocation
-      ! write(*, '(/,T5,A,/)') "Points de collocation :"
-      DO i = 1, N
-          write(*, '(1X, F10.5, 1X, F10.5)') x_c(i), y_c(i)
-      END DO
-
-      ! Initialisation des ondelettes de Haar
-      DO j = 1, N
-          DO i = 1, N
-              h(i,j) = haar(i, x_c(j), x_min, x_max, N)
-          END DO
-      END DO
-
-      ! Affichage des ondelettes de Haar
-      write(*, '(/,T5,A,/)') "Fonctions Haar :"
-      call print_matrix(h)
-
-      ! Affichage des intégrales premières
-      write(*, '(/,T5,A,/)') "Intégrales premières :"
-      DO j = 1, N
-          DO i = 1, N
-              p1(i,j) = P_haar(x_c(j), i, 1, x_min, x_max, N)
-          END DO
-      END DO
-      call print_matrix(p1)
-
-      ! Affichage des intégrales secondes
-      write(*, '(/,T5,A,/)') "Intégrales secondes :"
-      DO j = 1, N
-          DO i = 1, N
-              p2(i,j) = P_haar(x_c(j), i, 2, x_min, x_max, N)
-          END DO
-      END DO
-      call print_matrix(p2)
 
 
 
