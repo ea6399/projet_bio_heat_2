2ba5069986dd9226f703947fc67d0587c5e2353f
ALI Emin <eminali2430@gmail.com>
2025-04-10 09:38:18 +0200

    Test commit

---
+++ main.f90
@@ -0,0 +1,10 @@
+PROGRAM main
+      USE :: numrecis
+
+      IMPLICIT NONE
+
+      ! This is the main program
+
+    
+
+END PROGRAM main
---
+++ numerics.f90
@@ -0,0 +1,9 @@
+MODULE numerics
+
+      ! This module contains the global variables declarations
+
+      IMPLICIT NONE
+      INTEGER, PARAMETER :: rp = REAL(KIND=8)
+
+      
+END MODULE numerics
