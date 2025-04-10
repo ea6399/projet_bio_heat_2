9b8aaa892b0775df44194a425148e420c101d7d4
ALI Emin <eminali2430@gmail.com>
2025-04-10 10:04:55 +0200

    Initialise toutes les variables globales

--- main.f90
+++ main.f90
@@ -1,9 +1,14 @@
 PROGRAM main
-      USE :: numrecis
+
+      USE numerics
 
       IMPLICIT NONE
 
       ! This is the main program
+      !Initialisation des variables
+      CALL initialize_globals()
+      
+      write(*, '(/,T5,A,/)') " Toutes les variables globales ont été initialisées avec succès !"
 
     
 
